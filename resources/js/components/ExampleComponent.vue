f110a36545aee986dde7e1aac605e1a7bbf5fec1
Weylland <samiernicolas62@gmail.com>
2019-03-15 17:06:07 +0100

    First commit'

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <p>
+            <input type="text" v-model="name">
+            <input type="checkbox" v-model="checkbox">
+        </p>
+        <p>
+            Bonjour {{ name }}
+        </p> 
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                'name': 'Nicolas',
+                'checkbox': true,
+            }
+        }
+    }
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            <password-input></password-input>
+        </div>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
